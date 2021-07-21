0adf6b90b757012a7ebda7ecfad55168a8045c03
Pedrohme <pedrohmen45@gmail.com>
2021-07-21 20:44:48 -0300

    feat: wrote Menu class and finished Main program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using DIO.TVShows.Classes;
 
-namespace DIO.TVShows
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace DIO.TVShows {
+    class Program {
+        static void Main(string[] args) {
+            ShowRepository repository = new ShowRepository();
+            Menu menu = new Menu(repository);
+
+            menu.MainLoop();
         }
     }
 }
